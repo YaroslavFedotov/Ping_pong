26b9e992fece5ef3a591b9eaa8d3d3293271dc1a
YaroslavFedotov <56696549+YaroslavFedotov@users.noreply.github.com>
2021-02-28 23:57:32 +0300

    Initial commit

---
+++ Ping-pong/Ping-pong/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ping_pong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
