cd91d4675bfd69c37fd1ccc9cc4fb4098ed0f489
YaroslavFedotov <56696549+YaroslavFedotov@users.noreply.github.com>
2021-03-01 14:01:48 +0300

    Create combining client and server

--- Ping-pong/Ping-pong/Program.cs
+++ Ping-pong/Ping-pong/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Reflection;
+using System.Diagnostics;
 
 namespace Ping_pong
 {
@@ -11,21 +13,88 @@ namespace Ping_pong
     {
         static void Main(string[] args)
         {
-            TcpListener serverSocket = new TcpListener(IPAddress.Any, 7000);
-            Console.WriteLine("Server started");
-            serverSocket.Start();
-            TcpClient clientSocket = serverSocket.AcceptTcpClient();
-
-            NetworkStream stream = clientSocket.GetStream();
-            byte[] data = { 12, 13, 14, 15, 16, 44, 55, 66 };
-
-            stream.Write(data, 0, data.Length);
-            stream.Flush();
-
-            clientSocket.Close();
-            serverSocket.Stop();
-            Console.WriteLine("Server stopped");
+            Сonnection сonnection = null;
+            Console.WriteLine("сервер?");
+            string m = Console.ReadLine();
+            if (m == "y")
+            {
+                сonnection = new ServerСonnectionBehavior();
+            }
+            else if (m == "n")
+            {
+                сonnection = new ClientСonnectionBehavior();
+            }
+            else
+            {
+                Process.Start(Assembly.GetExecutingAssembly().Location);
+                Environment.Exit(0);
+            }
+            сonnection.TransferGameData();
         }
     }
+    abstract class Сonnection
+    {
+        abstract public void TransferGameData();
+    }
+
+    class ServerСonnectionBehavior : Сonnection
+    {
+        TcpListener serverSocket;
+        TcpClient clientSocket;
+        NetworkStream stream;
+        public ServerСonnectionBehavior()
+        {
+            serverSocket = new TcpListener(IPAddress.Any, 7000);
+            Console.WriteLine("Server started");
+            serverSocket.Start();
+            clientSocket = serverSocket.AcceptTcpClient();
+           // clientSocket.Close();
+            //serverSocket.Stop();
+            Console.WriteLine("Server stopped");
+        }
+        public override void TransferGameData()
+        {
+            stream = clientSocket.GetStream();
+            byte[] data = { 12, 13, 14, 15, 16, 44, 55, 66 };
+            stream.Write(data, 0, data.Length);
+            stream.Flush();
+        }
+    }
+    class ClientСonnectionBehavior : Сonnection
+    {
+        TcpClient client;
+        NetworkStream stream;
+        public ClientСonnectionBehavior()
+        {
+            try
+            {
+                client = new TcpClient("127.0.0.1", 7000);
+                stream = client.GetStream();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "1");
+            }
+        }
+        public override void TransferGameData()
+        {
+            try
+            {
+                byte[] data = new byte[256];
+                stream.Read(data, 0, data.Length);
+                foreach (byte b in data)
+                    Console.Write(b + " ");
+                client.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + "2");
+            }
+        }
+            
+    }
 }
+
+
 
