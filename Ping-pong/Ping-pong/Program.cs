2a7828124f6f80a6b1e981345e134c360b6a63c6
YaroslavFedotov <56696549+YaroslavFedotov@users.noreply.github.com>
2021-03-01 16:57:35 +0300

    Create performer

--- Ping-pong/Ping-pong/Program.cs
+++ Ping-pong/Ping-pong/Program.cs
@@ -14,35 +14,100 @@ namespace Ping_pong
         static void Main(string[] args)
         {
             Сonnection сonnection = null;
-            Console.WriteLine("сервер?");
-            string m = Console.ReadLine();
-            if (m == "y")
-            {
-                сonnection = new ServerСonnectionBehavior();
-            }
-            else if (m == "n")
-            {
-                сonnection = new ClientСonnectionBehavior();
-            }
-            else
-            {
-                Process.Start(Assembly.GetExecutingAssembly().Location);
-                Environment.Exit(0);
-            }
-            сonnection.TransferGameData();
+            char[,] playingField = new char[30, 120];
+            Console.CursorVisible = false;
+            GamePerformer.GameShow();
+            // Console.WriteLine("сервер?");
+            //string m = Console.ReadLine();
+            //if (m == "y")
+            //{
+            //    сonnection = new ServerСonnection();
+            // }
+            // else if (m == "n")
+            // {
+            //     сonnection = new ClientСonnection();
+            // }
+            // else
+            //  {
+            //     Process.Start(Assembly.GetExecutingAssembly().Location);
+            //     Environment.Exit(0);
+            //  }
+            // сonnection.TransferGameData();
         }
     }
+
+    static class GamePerformer
+    {
+        static char[,] playingField = new char[30, 120];
+        public static void GameShow()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                for (int j = 0; j < 120; j++)
+                {
+                    playingField[i, j] = ' ';
+                }
+            }
+
+            playingField[15, 60] = '@';
+            playingField[15, 61] = '@';
+            playingField[15, 59] = '@';
+            playingField[14, 60] = '@';
+            playingField[16, 60] = '@';
+
+            playingField[13, 0] = '#';
+            playingField[14, 0] = '#';
+            playingField[15, 0] = '#';
+            playingField[16, 0] = '#';
+            playingField[17, 0] = '#';
+            playingField[18, 0] = '#';
+            playingField[13, 1] = '#';
+            playingField[14, 1] = '#';
+            playingField[15, 1] = '#';
+            playingField[16, 1] = '#';
+            playingField[17, 1] = '#';
+            playingField[18, 1] = '#';
+
+            playingField[3, 118] = '#';
+            playingField[4, 118] = '#';
+            playingField[5, 118] = '#';
+            playingField[6, 118] = '#';
+            playingField[7, 118] = '#';
+            playingField[8, 118] = '#';
+            playingField[3, 119] = '#';
+            playingField[4, 119] = '#';
+            playingField[5, 119] = '#';
+            playingField[6, 119] = '#';
+            playingField[7, 119] = '#';
+            playingField[8, 119] = '#';
+
+            foreach (char c in playingField)
+                Console.Write(c);
+        }
+    }
+
+    class UserPlate
+    { 
+
+    }
+
+    class OpponentPlate
+    {
+
+    }
+
+
     abstract class Сonnection
     {
         abstract public void TransferGameData();
     }
 
-    class ServerСonnectionBehavior : Сonnection
+    class ServerСonnection : Сonnection
     {
         TcpListener serverSocket;
         TcpClient clientSocket;
         NetworkStream stream;
-        public ServerСonnectionBehavior()
+        public ServerСonnection()
         {
             serverSocket = new TcpListener(IPAddress.Any, 7000);
             Console.WriteLine("Server started");
@@ -60,11 +125,11 @@ namespace Ping_pong
             stream.Flush();
         }
     }
-    class ClientСonnectionBehavior : Сonnection
+    class ClientСonnection : Сonnection
     {
         TcpClient client;
         NetworkStream stream;
-        public ClientСonnectionBehavior()
+        public ClientСonnection()
         {
             try
             {
@@ -73,7 +138,7 @@ namespace Ping_pong
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message + "1");
+                Console.WriteLine(e.Message);
             }
         }
         public override void TransferGameData()
@@ -89,7 +154,7 @@ namespace Ping_pong
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message + "2");
+                Console.WriteLine(e.Message);
             }
         }
             
